2ed76189595218572ac8815e273ce899df06b8a8
bruh0422 <happyh960422@gmail.com>
2024-06-02 10:21:46 +0800

    將主檔案重新命名為 run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-import os
-import yt_dlp
+import os, yt_dlp
 
 ydl_opts = {
     'format': 'wav/bestaudio/best',
@@ -7,13 +6,14 @@ ydl_opts = {
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'wav'
-    }]
+    }],
+    'ignoreerrors': True
 }
 
 for name in ['output']:
     if name not in os.listdir():
-        print(f"缺少 {name} 資料夾。")
-        exit()
+        os.mkdir(name)
+        print(f"創建 {name} 資料夾...")
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download(input('輸入影片或播放清單連結: ').split())
